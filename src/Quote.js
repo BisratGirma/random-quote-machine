145411dc91b47701d00f2b6e1d8a09e00805712c
Bisrat Girma <bisratx@gmail.com>
2022-06-10 12:29:50 +0300

    finalized added thousand quotes and colors

--- src/Quote.js
+++ src/Quote.js
@@ -1,26 +1,29 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import './App.scss';
+import quoteList from './quotes_list';
+import colorList from './colorList';
 
 function Quote() {
-  const quoteBank = [{
-    author: 'Dalia lama',
-    quote: 'The purpose of our lives is to be happy.'
-  },
-  {
-    author: 'Nelson Mandela'
-    , quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall.'
-  }
-  ];
+  const colors = colorList;
+  const quoteBank = quoteList;
   const randomGenerator = () => Math.floor(Math.random() * quoteBank.length);
+  const randomColor = () => Math.floor(Math.random() * colors.length);
+
   let quoteNumber = randomGenerator();
-  const [quote, randomQuote] = useState(quoteBank[quoteNumber].quote);
+  let colorNumber = randomColor();
+  const [quote, randomQuote] = useState(quoteBank[quoteNumber].text);
   const [author, randomAuthor] = useState(quoteBank[quoteNumber].author);
+  const [color, colorPicker] = useState(colors[colorNumber].hex);
   const randomAuthorAndQuote = () => {
     let random = randomGenerator();
     if (random !==  quoteNumber){
       quoteNumber = random
-      randomQuote(quoteBank[quoteNumber].quote)
+      randomQuote(quoteBank[quoteNumber].text)
       randomAuthor(quoteBank[quoteNumber].author)
+      colorNumber = randomColor()
+      colorPicker(colors[colorNumber].hex)
       console.log(quoteNumber)
     } else {
       randomAuthorAndQuote()
@@ -32,14 +35,18 @@ function Quote() {
 
   return (
     <div className='App'>
-      <div id="quote-box">
+      <header className='header' style={{backgroundColor: color, color: color}}>
+      <div id="quote-box" >
       <p id="text">{quote}</p>
       <p id="author">- {author}</p>
-      <a id="tweet-quote" href='twitter.com/intent/tweet' _blank>T</a>
-      <button id="new-quote" onClick={()=> {
+      <a id="tweet-quote" href='https://twitter.com/intent/tweet' target='_blank' rel="noreferrer" style={{backgroundColor: color}}>
+      <FontAwesomeIcon icon={faTwitter} />
+      </a>
+      <button id="new-quote" style={{backgroundColor: color}} onClick={()=> {
         randomAuthorAndQuote()
         }}>New Quote</button>
     </div>
+      </header>
     </div>
   );
 }
