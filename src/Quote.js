03318beacbee30a5b35c94a31ca5749a67edc543
bisrat <30285329+bisrax@users.noreply.github.com>
2022-06-12 14:20:28 +0300

    Update Quote.js

--- src/Quote.js
+++ src/Quote.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
 //import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 //import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import TwitterIcon from 'https://cdn.skypack.dev/@mui/icons-material/Twitter';
 import './App.scss';
 import quoteList from './quotes_list';
 import colorList from './colorList';
+//var { useState } = 'react';
 
 function Quote() {
   const colors = colorList;
