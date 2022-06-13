cdc0b024d881398b315920d6c215a158d84b6b35
bisrat <30285329+bisrax@users.noreply.github.com>
2022-06-13 14:30:14 +0300

    simple

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-const twitterIcon = require('https://cdn.skypack.dev/@mui/icons-material/Twitter');
-require('./src/App.scss');
-require('./src/index.css');
-const  quoteList = require('./src/quotes_list');
-const colorList = require('./src/colorList');
+import React, { useState } from "https://cdn.skypack.dev/react";
+import * as ReactDOM from "https://cdn.skypack.dev/react-dom";
+//import * as fetch from "https://cdn.skypack.dev/fetch@1.1.0";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Quote />
-  </React.StrictMode>
-);
 function Quote() {
-  const colors = colorList;
-  const quoteBank = quoteList;
+  const colors = [
+    "#16a085",
+    "#27ae60",
+    "#2c3e50",
+    "#f39c12",
+    "#e74c3c",
+    "#9b59b6",
+    "#FB6964",
+    "#342224",
+    "#472E32",
+    "#BDBB99",
+    "#77B1A9",
+    "#73A857"
+  ];
+  let quoteBank = [];
+  fetch("https://raw.githubusercontent.com/bisrax/random-quote-machine/master/src/quotes_list.json").then((response) => response.json()).then((data) => {
+    quoteBank = data.map((v, k) => data[k])
+  });
+  console.log(quoteBank);
   const randomGenerator = () => Math.floor(Math.random() * quoteBank.length);
   const randomColor = () => Math.floor(Math.random() * colors.length);
 
@@ -20,38 +29,51 @@ function Quote() {
   let colorNumber = randomColor();
   const [quote, randomQuote] = useState(quoteBank[quoteNumber].text);
   const [author, randomAuthor] = useState(quoteBank[quoteNumber].author);
-  const [color, colorPicker] = useState(colors[colorNumber].hex);
+  const [color, colorPicker] = useState(colors[colorNumber]);
   const randomAuthorAndQuote = () => {
     let random = randomGenerator();
-    if (random !==  quoteNumber){
-      quoteNumber = random
-      randomQuote(quoteBank[quoteNumber].text)
-      randomAuthor(quoteBank[quoteNumber].author)
-      colorNumber = randomColor()
-      colorPicker(colors[colorNumber].hex)
-      console.log(quoteNumber)
+    if (random !== quoteNumber) {
+      quoteNumber = random;
+      randomQuote(quoteBank[quoteNumber].text);
+      randomAuthor(quoteBank[quoteNumber].author);
+      colorNumber = randomColor();
+      colorPicker(colors[colorNumber]);
+      console.log(quoteNumber);
     } else {
-      randomAuthorAndQuote()
-    }  
-  }
+      randomAuthorAndQuote();
+    }
+  };
 
   return (
-    <div className='App'>
-      <header className='header' style={{backgroundColor: color, color: color}}>
-      <div id="quote-box" >
-      <p id="text">{quote}</p>
-      <p id="author">- {author}</p>
-      <a id="tweet-quote" href='https://twitter.com/intent/tweet' target='_blank' rel="noreferrer" style={{backgroundColor: color}}>
-        {//<FontAwesomeIcon icon={faTwitter} />}
-        {twitterIcon}
-      }
-      </a>
-      <button id="new-quote" style={{backgroundColor: color}} onClick={()=> {
-        randomAuthorAndQuote()
-        }}>New Quote</button>
-    </div>
+    <div className="App">
+      <header
+        className="header"
+        style={{ backgroundColor: color, color: color }}
+      >
+        <div id="quote-box">
+          <p id="text">{quote}</p>
+          <p id="author">- {author}</p>
+          <a
+            id="tweet-quote"
+            href="https://twitter.com/intent/tweet"
+            target="_blank"
+            rel="noreferrer"
+            style={{ backgroundColor: color }}
+          >
+            Tweet
+          </a>
+          <button
+            id="new-quote"
+            style={{ backgroundColor: color }}
+            onClick={() => {
+              randomAuthorAndQuote();
+            }}
+          >
+            New Quote
+          </button>
+        </div>
       </header>
     </div>
   );
 }
-    
+ReactDOM.render(<Quote />, document.getElementById("root"));
